ff5389bfa324f31fcba4c22ab9fdcf73692db01f
Shafin Ahmed <kazishafin@gmail.com>
2025-05-18 18:36:31 -0400

    Add WIP header

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+
+export default function Header() {
+  return (
+    <header className="flex items-center justify-between p-4">
+      <div className="flex items-center space-x-2">
+        <Image src="/logo.svg" alt="SquadGoals Logo" width={32} height={32} />
+        <h1 className="text-2xl font-bold">SquadGoals</h1>
+      </div>
+      <nav>
+        <ul className="flex space-x-4">
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/challenges">Challenges</a>
+          </li>
+          <li>
+            <a href="/squads">Squads</a>
+          </li>
+          <li>
+            <a href="/signup">Sign Up</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
